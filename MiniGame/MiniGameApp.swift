97f65ba8ee8ca0eebf598448ffecf02933a85bb6
Ryoma <iitotyan@gmail.com>
2022-06-15 22:50:51 +0900

    Initial Commit

---
+++ MiniGame/MiniGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniGameApp.swift
+//  MiniGame
+//
+//  Created by 小林龍馬 on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct MiniGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
